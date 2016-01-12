b3dd3d077acad7e1d1098e2c154ceea38e00fd5f
Cellvin Chung <cellvinchung@gmail.com>
2016-01-12 10:00:54 +0800

    20160112v1.1

--- README.md
+++ README.md
@@ -3,18 +3,19 @@ ckeditor for playplus, based on ckeditor 4.5.6<br>
 使用時需要引入的js:<br>
 ckeditor.js<br>
 playplus_ckeditor.js<br>
-/plugins/liststyle/dialogs/liststyle.js<br>
 <br>
 playplus_ckeditor預設設定：<br>
 ・樣式：office word 2013<br>
-・表格載入寬：100%<br>
+・表格載入預設寬：100%<br>
+・簡化圖片上傳對話框<br>
+・可拖曳表格中儲存格欄寬<br>
 ・語系：中文<br>
 ・字級：最小12px<br>
 ・字型：新增微軟正黑體、標楷體、新細明體<br>
-・order list：新增小寫中文(一、二、三)<br>
+・order list：新增小寫中文數字(一、二、三)<br>
 ・新增行高：0.3~5<br>
 ・新增字元間距：-10px~10px<br>
-・刪除flash、預覽<br>
-
-其他客製化請至playplus_ckeditor.js編輯<br>
+・准許原始碼模式中寫class、id等html屬性及inline style
+・刪除flash、預覽、表單外掛<br>
+其他客製化設定請至playplus_ckeditor.js編輯<br>
 參考內容：http://docs.ckeditor.com/#!/api/CKEDITOR.config-cfg-fileTools_defaultFileName
--- ckeditor/playplus_ckeditor.js
+++ ckeditor/playplus_ckeditor.js
@@ -8,7 +8,10 @@
 //禁止文字，ckeditor 4 以上版本已預設禁止script，參考http://docs.ckeditor.com/#!/guide/dev_disallowed_content
 //CKEDITOR.config.disallowedContent = 'script; *[on*]';
 
-//table表格寬預設100%
+//准許文字，可以在原始碼模式寫class與id等屬性以及inline style
+CKEDITOR.config.extraAllowedContent = '*(*);*{*}';
+
+//table表格寬預設100%，圖片上傳寬100%
 CKEDITOR.on('dialogDefinition', function(ev) {
 
 	var diagName = ev.data.name;
@@ -19,6 +22,13 @@ CKEDITOR.on('dialogDefinition', function(ev) {
 
 		var width = infoTab.get('txtWidth');
 		width['default'] = "100%";
+
+	}
+	if (diagName === 'image2') {
+		var infoTab = diagDefn.getContents('info');
+
+		// var width = infoTab.get('txtWidth');
+		// width['default'] = "100%";
 
 	}
 });
@@ -35,11 +45,25 @@ CKEDITOR.config.font_names="微軟正黑體/微軟正黑體;標楷體/標楷體;
 //行高
 CKEDITOR.config.line_height="0.3;0.4;0.5;0.6;0.7;0.8;0.9;1;1.1;1.2;1.3;1.4;1.5;2;3;4;5";
 
+//載入其他外掛
+CKEDITOR.config.extraPlugins = 'image2,liststyle,filetools,notificationaggregator,notification,uploadwidget,uploadimage';
+
+//移除外掛
+CKEDITOR.config.removePlugins = 'save,language, iframe, forms';
+
+//回復次數上限，預設20
+//CKEDITOR.config.undoStackSize = 20;
+
+//每次縮排大小，預設40
+//CKEDITOR.config.indentOffset = 40;
+//縮排單位，預設px
+//CKEDITORconfig.indentUnit = 'px';
+
+//輔助線顏色，預設#FF0000
+//CKEDITOR.config.magicline_color = '#FF0000';
+
 //載入輸出目標頁面的css到編輯器內，減少前後台誤差
 //CKEDITOR.config.contentsCss = '/css/mysitestyles.css';
 
 //載入特定js檔至特定的textarea id，用id名稱置換myfield
 //CKEDITOR.replace( 'myfield', { customConfig: '/myconfig.js' } );
-
-//載入外掛
-//CKEDITOR.config.extraPlugins = 'myplugin,anotherplugin';
